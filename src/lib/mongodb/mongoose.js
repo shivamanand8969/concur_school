24f56b96dde43d7ec6fc25a739b6524c3e7a17ac
shivam anand <shivamanand@shivams-MacBook-Air.local>
2026-05-17 13:19:16 +0530

    updated

--- src/lib/mongodb/mongoose.js
+++ src/lib/mongodb/mongoose.js
@@ -10,7 +10,7 @@ export const connect = async () => {
   }
   try {
     await mongoose.connect(process.env.MONGODB_URI, {
-      dbName: 'next-blog',
+      dbName: 'concur_school',
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
